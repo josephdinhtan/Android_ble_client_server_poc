f43d55a11e07ceb21978f2bd54b3963b28568010
Joseph Dinh Tan <josephdinhtan@gmail.com>
2023-05-01 18:18:33 +0700

    Integrate Central, Peripheral code which able to connect each other

---
+++ ble_accy_peripheral/src/main/java/com/example/ble_accy_perif/BleUtilities.kt
@@ -0,0 +1,12 @@
+package com.example.ble_accy_perif
+
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.ActivityCompat.startActivityForResult
+
+private const val ENABLE_BLUETOOTH_REQUEST_CODE = 1
+
+object BleUtilities {
+}
